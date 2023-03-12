a96a5b6b5bc09033ed4eaadd892e1dc4d3d4116e
FredyCansecoSantos <100893800+FredyCansecoSantos@users.noreply.github.com>
2023-03-12 16:27:45 -0600

    Add files via upload

--- Challenge 2/catkin_challenge/pid_control/scripts/set_point_generator.py
+++ Challenge 2/catkin_challenge/pid_control/scripts/set_point_generator.py
@@ -1,40 +1,60 @@
 #!/usr/bin/env python
+
 import rospy
 import numpy as np
 from pid_control.msg import set_point
 
-# Setup Variables, parameters and messages to be used (if required)
-#input = rospy.get_param("/input",1.0)
-#time = rospy.get_param("/time",0.0)
+#Se realiza el seteo de la variables para generar la senhal senoidal
+#Estos parametros incluyen la amplitud y la frecuencia
+Amplitud = rospy.get_param("/setPoint_Amplitude",3.0)
+Omega = rospy.get_param("/setPoint_freq",10.0)
+
+# Setup Variables and messages to be used
+ini_input = set_point()
+ini_input.time = 0.0
+ini_input.entrada = 0.0
+
+#Define variables to be used
+first = True
+start_time = 0.0
+signal = 0.0
+time = 0.0
 
 #Stop Condition
 def stop():
- #Setup the stop message (can be the same as the control message)
-  print("Stopping")
+
+#Setup the stop message (can be the same as the control message)
+ print("Stopping")
 
 if __name__=='__main__':
+
     #Initialise and Setup node
+    signal_pub = rospy.Publisher("/set_point",set_point, queue_size=1)
+
     rospy.init_node("Set_Point_Generator")
+
     rate = rospy.Rate(100)
+
     rospy.on_shutdown(stop)
 
     #Setup Publishers and subscribers here
-    signal_pub = rospy.Publisher("/set_point",set_point, queue_size=1)
-    #time_pub = rospy.Publisher("set_point",set_point, queue_size=1)
-
     print("The Set Point Generator is Running")
 
-    Amplitud = rospy.get_param("/setPoint_Amplitude",8.0)
-    Omega = rospy.get_param("/setPoint_freq",0.1)
     start_time = rospy.get_time()
-    ini_input = set_point()
+
     #Run the node
+
     while not rospy.is_shutdown():
-        time = 0
+
         time = rospy.get_time()-start_time
+        #Con los parametros anteriores generamos la senhal senoidal
         signal = Amplitud*np.sin(Omega*time)
+
+        #Publicamos la senhal al topico de /set_point
+
+        ini_input.entrada = signal
+
         ini_input.time = time
-        ini_input.input = signal
 
         signal_pub.publish(ini_input)
 
