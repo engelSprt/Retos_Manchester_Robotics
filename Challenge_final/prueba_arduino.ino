37d41a64458f91ffeac5eed3b09f048e3ef179d3
FredyCansecoSantos <100893800+FredyCansecoSantos@users.noreply.github.com>
2023-03-15 06:47:26 -0600

    Add files via upload

--- Challenge_final/prueba_arduino.ino
+++ Challenge_final/prueba_arduino.ino
@@ -1,130 +1,149 @@
-//NODO de ROS en Arduino
-
 #include <ros.h>
 #include <std_msgs/Float32.h>
-#include <std_msgs/Int64.h> //Incluir las librerias para los mensajers customizados
-#include <std_msgs/Int16.h>
-#include <motor_input.h>
-#include <motor_output.h>
-#define MOTOR 0
-#define CW    1 //Clock wise
-#define CCW   2 //Counter clock wise
-#define STOP 0
-//Pins entre arduino y puente h del diagrama de conexiones de MCR2
-#define PIN1_MOTOR #
-#define PIN2_MOTOR #
-#define PWM #
-//pins para arduino
-const int Pin1_encoder = 2;
-const int Pin2_encoder = 3;
-//valor del registro puede cambiar a través de acciones fuera del programa
-volatile int64_t position = 0;
+#include <digitalWriteFast.h>
 
-//Los mensajes del encoder se envian a traves de un mensaje customizado para que el nodo dentro de la computadora pueda calcular y enviar cada uno
-//De los parametros de velocidad angular.
+//Declaracion de los pines
+const int EncA = 2;
+const int EncB = 3;
+const int In1 = 4; // Digital output pin
+const int In2 = 5; // Digital output pin
+const int EnA = 6; // PWM output pin
+float conversion, posicion, posactual = 0, posanterior = 0, velocidad = 0;
+float resolucion = 0.0306;  //Definir resolución del encoder
+int pulsos = 111789      //Número de pulsos a la salida del motorreductor
 
-//El mensaje de la respuesta PID publica a la entrada del motor mediante un mensaje customizado
+String dir = "CW", last_dir = "CCW";
+long current_time = 0;
+long last_time = 0;
+volatile bool lecA = 0;
+volatile bool lecB = 0;
 
-//lib ros
-ros::NodeHandle  nh;
+long contador = 0, contaux = 0;
+float dt = 0.02
 
-//define el tipo de datos del topic.
-custom_msg::motor_output sensor_encoder;
+void leerA(){
+  lecA = digitalReadFast(EncA);
+  lecB = digitalReadFast(EncB);
 
-//publicador
-ros::Publisher Value_position("Value_position", &sensor_encoder);
-
-//suscriptor para el tema /motor_input.
-ros::Subscriber<custom_msg::motor_input> pwm_motor("motor_input", &signal_pwm);
-
-//funcion para prender el motor con los parámtros:  motor(0 ó 1), direccion cw ó ccw ,  pwm (0 - 255)
-void motor_on(int16_t motor, int16_t direction, Float32 pwm){
-
-  if(motor == MOTOR){
-
-    if(direction == CW){
-      digitalWrite(PIN1_MOTOR, LOW);
-      digitalWrite(PIN2_MOTOR, HIGH);
+  if(lecA != lecB){
+    contador ++;
+    contaux++;
+    posicion = contador * resolucion; //Convertir a grados
+    posactual = contaux * resolucion;
+    if(contador >= pulsos)
+    {
+      contador = 0;
     }
-    else if(direction == CCW){
-      digitalWrite(PIN1_MOTOR, HIGH);
-      digitalWrite(PIN2_MOTOR, LOW);
-    }
-    else{
-      digitalWrite(PIN1_MOTOR, LOW);
-      digitalWrite(PIN2_MOTOR, LOW);
-    }
-
-   analogWrite(PWM, pwm);
   }
-
+  else
+  {
+  contador --;
+  contaux--;
+  posicion = contador * resolucion;
+  posactual = contaux * resolucion; //Convertir a grados
+  if(contador <= -pulsos)
+  {
+    contador = 0;
+  }
+ }
 }
-//funciones  para leer los valores del encoder
-//funciones de rutina de servicio de interrupción para actualizar el valor del encoder sobre el movimiento del eje del motor
-void Encoder_out1(){
-  if (digitalRead(Pin1_encoder) != digitalRead(Pin2_encoder)){
-    position++;
+
+void leerB(){
+  lecA = digitalReadFast(EncA);
+  lecB = digitalReadFast(EncB);
+
+  if(lecA == lecB){
+    contador ++;
+    contaux++;
+    posicion = contador * resolucion; //Convertir a grados
+    posactual = contaux * resolucion;
+    if(contador >= pulsos)
+    {
+      contador = 0;
+    }
   }
   else{
-    position--;
+  contador --;
+  contaux--;
+  posicion = contador * resolucion;
+  posactual = contaux * resolucion; //Convertir a grados
+  if (contador <= -pulsos) //Contar por revoluciones
+    {
+      contador=0;
   }
-}
-void Encoder_out2(){
-  if (digitalRead(Pin1_encoder) == digitalRead(Pin2_encoder)){
-    position++;
-  }
-  else{
-    position--;
-  }
-}
-
-
-// función para mover el motor de acuerdo con el signo del valor PWM que se le da
-void signal_pwm( const custom_msg::motor_input &value_pwm){
-  Float32 pwm =0;
-  pwm = value_pwm.input;
-
-  if ( pwm > 0 ){
-  motor_on(MOTOR,CCW,pwm);
-  }
-  else{
-  motor_on(MOTOR,CW,abs(pwm));
-  }
-
+ }
 }
 
-//setupp de configuración de los pines del Encoder como pines de interrupción.
-void setup(){
-  //lib ros
+void pwm_int(const std_msgs::Float32& msg) {
+
+  float val_received = msg.data;
+
+  if (val_received > 0) {
+    dir = "CW";
+  }
+  else {
+    dir = "CCW";
+  }
+
+  if (dir != last_dir) {
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);
+    delay(100);
+  }
+
+  last_dir = dir;
+
+  int pwm = int(val_received*255);
+  //int pwm = map(val_received, -1, 1, -255, 255);
+  if (pwm >= 0 && pwm <= 255) {
+    digitalWrite(In1, HIGH);
+    digitalWrite(In2, LOW);
+    analogWrite(EnA, pwm);
+  }
+  else if (pwm >= -255 && pwm <= 0) {
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, HIGH);
+    analogWrite(EnA, -pwm);
+  }
+
+}
+
+ros::NodeHandle nh;
+std_msgs::Float32 velocidad_out
+ros::Subscriber<std_msgs::Float32> motor("motor_input", pwm_int);
+ros::Publisher motor_output("motor_output", &velocidad_out);
+
+void setup() {
   nh.initNode();
-  nh.advertise(Value_position);
-  nh.subscribe(pwm_motor);
+  nh.subscribe(motor);
+  nh.advertise(motor_output);
 
-  //Se le agrega al pin una resistencia de pull-up interna y se configura como entrada a los pines de interrupcion.
-  pinMode (Pin1_encoder, INPUT_PULLUP);
-  pinMode (Pin2_encoder, INPUT_PULLUP);
+  pinMode(EnA, OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
 
-  //registro de temporizador para modificar velocidades del PWM ATMEGA328 - arduino UNO
-  //timers para interrupción
+  pinMode(EncA, INPUT_PULLUP);
+  pinMode(EncB, INPUT_PULLUP);
 
-  //timer1 - PWM frequencia = 490.20 Hz en pines:  D9 & D10
-  //TCCR1B = TCCR1B & B11111000 | B00000011;
-
-  //timer2 - PWM frequencia = 490.20 Hz en pines:  D3 & D11 para que sea más raṕida la interrupcion
-  TCCR2B = TCCR2B & B11111000 | B00000100;
-
-  //las interrupciones por "cambio" que nos recomendó el profe
-  attachInterrupt (digitalPinToInterrupt (Pin1_encoder), Encoder_out1, CHANGE);
-  attachInterrupt (digitalPinToInterrupt (Pin2_encoder), Encoder_out2, CHANGE);
+  attachInterrupt (digitalPinToInterrupt (EncA), leerA, CHANGE);
+  attachInterrupt (digitalPinToInterrupt (EncB), leerB, CHANGE);
 }
 
-//loop para publicar continuamente los valores del Encoder actualizados en el topic definido.
-void loop(){
-  //lib ros
-  nh.loginfo("Encoder data");
-  sensor_encoder.output = position;
-  Value_position.publish( &sensor_encoder );
+void loop() {
+  current_time = millis();
+  // Period of time "interval"
+  if (current_time - last_time > dt) {
+    last_time = current_time;
+    //Método de Euler para la estimación de la velocidad
+    velocidad = ((posactual - posanterior) / 0.015); //Tiempo de muestreo = 0.015 segundos
+    //Se pregunta por la velocidad, cuando hay una inversion de giro, para hacerla positiva
+    if (velocidad < 0){
+      velocidad = abs(velocidad);
+    //Actualizar la lectura de la posición anterior
+      posanterior = posactual;
+    }
+  }
+  velocidad_out.data = velocidad;
+  motor_output.publish(&velocidad_out)
   nh.spinOnce();
-  delay(100);
-
 }
